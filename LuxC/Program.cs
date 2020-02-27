63a76b1776bb4479c820f657deb1ef6b6576b002
V. Daemond Antonio <DaeAnt@gmail.com>
2020-02-26 19:55:20 -0700

    Engine and Game setup

--- LuxC/Program.cs
+++ LuxC/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LuxC.model;
 using ConsoleGameEngine;
+using LuxC.Controller;
 
 namespace LuxC
 {
@@ -12,36 +13,8 @@ namespace LuxC
     {
         static void Main(string[] args)
         {
-            ConsoleEngine graphicsEngine = new ConsoleEngine(240,132,8,8);
-            Drawable.setEngine(graphicsEngine);
-            //graphicsEngine.Borderless();
-            graphicsEngine.SetBackground(8);
-
-            Rondure v = new Rondure(ConsoleColor.Blue);
-            v.Position = new Point(10, 12);
-            v.Draw();
-
-            v.setColor(ConsoleColor.Red);
-            v.Position = new Point(10, 18);
-            v.Draw();
-
-            v.setColor(ConsoleColor.White);
-            v.Position = new Point(16, 12);
-            v.Draw();
-
-            v.setColor(ConsoleColor.Black);
-            v.Position = new Point(16, 18);
-            v.Draw();
-
-            PaletteSprite palette = new PaletteSprite();
-            palette.Position = new Point(0, 0);
-            palette.Draw();
-
-            Path path = new Path();
-            path.Draw();
-            Console.Read();
-            graphicsEngine.DisplayBuffer();
-            Console.Read();
+            Lux lux = new Lux();
+            lux.Construct(240, 132, 8, 8, FramerateMode.MaxFps);
         }
     }
 }
