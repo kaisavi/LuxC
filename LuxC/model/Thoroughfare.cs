eca454dd4b80c3f07b825600793488275cc4c01a
V. Daemond Antonio <DaeAnt@gmail.com>
2020-02-26 21:35:04 -0700

    Leprechauns!

---
+++ LuxC/model/Thoroughfare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleGameEngine;
+namespace LuxC.model
+{
+    public class Thoroughfare : Drawable
+    {
+        List<Point> points = new List<Point>();
+        public Thoroughfare(List<Point> controlPoints) {
+            points = new BezierCurve().Bezier2D(controlPoints);
+        }
+
+
+        public override void Draw()
+        {                                      
+            foreach(Point p in points) {
+                engine.SetPixel(p, (int)ConsoleColor.DarkBlue,ConsoleCharacter.Light);
+            }
+        }
+    }
+}
