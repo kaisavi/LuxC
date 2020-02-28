9ba056cef6a541920157a81e3c0e60553a31bd76
V. Daemond Antonio <DaeAnt@gmail.com>
2020-02-27 19:23:16 -0700

    Refactor OrderOfBusiness -> control

---
+++ LuxC/controller/OrderOfBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuxC.model;
+using ConsoleGameEngine;
+using LuxC.control;
+
+namespace LuxC.control
+{
+    class OrderOfBusiness
+    {
+        static void Main(string[] args)
+        {
+            Lux lux = new Lux();
+            lux.Construct(240, 132, 8, 8, FramerateMode.Unlimited);
+        }
+    }
+}
