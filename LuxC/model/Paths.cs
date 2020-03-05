682e742d83361c7c467edbbb34fd6c45f2b039d0
V. Daemond Antonio <DaeAnt@gmail.com>
2020-03-05 08:44:47 -0700

    Paths struct

---
+++ LuxC/model/Paths.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleGameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxC.model {
+    struct Paths {
+        internal static readonly Path demo = new Path(new List<Point> {
+            
+            new Point(-10,32),
+            
+            new Point(32,64),
+            new Point(64,75),
+            
+        });
+    }
+}
